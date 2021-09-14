c7b5d9600171eefe06f6d54f895ee008ad91af3d
suihaomiao <shm8283223@163.com>
2021-09-14 02:44:26 -0700

    second not complete!

--- main.c
+++ main.c
@@ -9,6 +9,27 @@ typedef struct {
     ssize_t input_length;
 }InputBuffer;
 
+//
+typedef enum{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+//
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}PreprareResult;
+
+typedef enum{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+}StatementType;
+
+typedef struct{
+    StatementType type;
+}Statement;
+
 //初始化一个新的buffer
 InputBuffer* new_input_buffer()
 {
@@ -46,6 +67,32 @@ void close_input_buffer(InputBuffer* input_buffer)
     free(input_buffer);
 }
 
+//
+MetaCommandResult do_meta_command(InputBuffer* inputbuffer){
+    if(strcmp(inputbuffer->buffer, ".exit") == 0){
+        exit(EXIT_SUCCESS);
+    }else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+//
+PreprareResult prepare_statement(InputBuffer* inputbuffer, Statement* statement){
+    if(strncmp(inputbuffer->buffer, "insert", 6) == 0){
+        statement->type == STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(inputbuffer->buffer, "select") == 0)
+    {
+        statement->type == STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+    
+}
+
+
 int main(int argc, char* argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
@@ -55,7 +102,7 @@ int main(int argc, char* argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ". exit") == 0)
+        if(strcmp(input_buffer->buffer, ".exit") == 0)
         {
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
@@ -64,6 +111,19 @@ int main(int argc, char* argv[])
         {
             printf("Unrecognized command '%s'\n",input_buffer->buffer);
         }
+
+        if(input_buffer->buffer[0] == '.'){
+            switch (do_meta_command(input_buffer))
+            {
+            case META_COMMAND_SUCCESS:
+                continue;
+            case META_COMMAND_UNRECOGNIZED_COMMAND:
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+            }
+        }
+
+
 
     }
     
